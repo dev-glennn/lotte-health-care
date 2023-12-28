5807d7f916636fc9af24fdd40f10e9b48c5c9398
Glenn <57659894+dev-glennn@users.noreply.github.com>
2023-12-29 01:10:04 +0900

    feat: 회원가입 요소 배치

--- src/pages/signUp/SignUp.tsx
+++ src/pages/signUp/SignUp.tsx
@@ -1,3 +1,20 @@
+import { Button, Input } from '../../components';
+import PText from '../../components/text/PText';
+import { SignUpContainer, SignUpContent } from './styles.css';
+
 export const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div className={SignUpContainer}>
+      <div className={SignUpContent}>
+        <PText>회원가입</PText>
+        이름
+        <Input />
+        이메일
+        <Input />
+        비밀번호
+        <Input />
+        <Button>회원가입</Button>
+      </div>
+    </div>
+  );
 };
---
+++ src/pages/signUp/styles.css.ts
@@ -0,0 +1,10 @@
+import { style } from '@vanilla-extract/css';
+
+export const SignUpContainer = style({
+  flex: 1,
+  overflowY: 'auto',
+});
+
+export const SignUpContent = style({
+  position: 'relative',
+});
